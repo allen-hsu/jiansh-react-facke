562466e08ed8de5541fc1a2796b27f2e411ab1e5
Allen Hsu <allenhsu.taiwan@gmail.com>
2018-12-01 16:05:42 +0800

    finish css animation

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import { CSSTransition } from "react-transition-group";
 import { actionCreators } from "./store";
@@ -17,77 +17,135 @@ import {
   SearchInfoItem,
   SearchInfoList
 } from "./style";
-const getListArea = show => {
-  if (show) {
-    return (
-      <SearchInfo>
-        <SearchInfoTitle>
-          熱門搜索
-          <SearchInfoSwitch>換一換</SearchInfoSwitch>
-        </SearchInfoTitle>
-        <SearchInfoList>
-          <SearchInfoItem>教育</SearchInfoItem>
-          <SearchInfoItem>教育</SearchInfoItem>
-          <SearchInfoItem>教育</SearchInfoItem>
-          <SearchInfoItem>教育</SearchInfoItem>
-          <SearchInfoItem>教育</SearchInfoItem>
-          <SearchInfoItem>教育</SearchInfoItem>
-        </SearchInfoList>
-      </SearchInfo>
-    );
-  } else {
-    return null;
+
+class Header extends Component {
+  getListArea() {
+    const {
+      focused,
+      list,
+      page,
+      totalPage,
+      mouseIn,
+      handleMouseEnter,
+      handleMouseLeave,
+      handleChangePage
+    } = this.props;
+    const newList = list.toJS();
+    const pageList = [];
+    if (newList.length) {
+      for (let i = (page - 1) * 10; i < page * 10; i++) {
+        pageList.push(
+          <SearchInfoItem key={newList[i]}>{newList[i]}</SearchInfoItem>
+        );
+      }
+    }
+
+    if (focused || mouseIn) {
+      return (
+        <SearchInfo
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
+          <SearchInfoTitle>
+            熱門搜索
+            <SearchInfoSwitch
+              onClick={() => handleChangePage(page, totalPage, this.spinIcon)}
+            >
+              <i
+                ref={icon => {
+                  this.spinIcon = icon;
+                }}
+                className="iconfont spin"
+              >
+                &#xe851;
+              </i>
+              換一批
+            </SearchInfoSwitch>
+          </SearchInfoTitle>
+          <SearchInfoList>{pageList}</SearchInfoList>
+        </SearchInfo>
+      );
+    } else {
+      return null;
+    }
   }
-};
-const Header = props => {
-  return (
-    <HeaderWrapper>
-      <Logo />
-      <Nav>
-        <NavItem className="left active">首頁</NavItem>
-        <NavItem className="left">下載</NavItem>
-        <NavItem className="right">登入</NavItem>
-        <NavItem className="right">
-          <i className="iconfont">&#xe636;</i>
-        </NavItem>
-        <SearchWrapper>
-          <CSSTransition in={props.focused} timeout={200} classNames="slide">
-            <NavSearch
-              className={props.focused ? "focused" : ""}
-              onFocus={props.handleInputFocus}
-              onBlur={props.handleInputBlur}
-            />
-          </CSSTransition>
-          <i className={props.focused ? "focused iconfont" : "iconfont"}>
-            &#xe614;
-          </i>
-          {getListArea(props.focused)}
-        </SearchWrapper>
-      </Nav>
-      <Addition>
-        <Button className="writting">
-          <i className="iconfont">&#xe670;</i>寫文章
-        </Button>
-        <Button className="reg">註冊</Button>
-      </Addition>
-    </HeaderWrapper>
-  );
-};
+  render() {
+    const { focused, handleInputFocus, handleInputBlur, list } = this.props;
+    return (
+      <HeaderWrapper>
+        <Logo />
+        <Nav>
+          <NavItem className="left active">首頁</NavItem>
+          <NavItem className="left">下載</NavItem>
+          <NavItem className="right">登入</NavItem>
+          <NavItem className="right">
+            <i className="iconfont">&#xe636;</i>
+          </NavItem>
+          <SearchWrapper>
+            <CSSTransition in={focused} timeout={200} classNames="slide">
+              <NavSearch
+                className={focused ? "focused" : ""}
+                onFocus={() => handleInputFocus(list)}
+                onBlur={handleInputBlur}
+              />
+            </CSSTransition>
+            <i className={focused ? "focused iconfont zoom" : "iconfont zoom"}>
+              &#xe614;
+            </i>
+            {this.getListArea()}
+          </SearchWrapper>
+        </Nav>
+        <Addition>
+          <Button className="writting">
+            <i className="iconfont">&#xe670;</i>寫文章
+          </Button>
+          <Button className="reg">註冊</Button>
+        </Addition>
+      </HeaderWrapper>
+    );
+  }
+}
 
 const mapStateToProps = state => {
   return {
-    focused: state.getIn(["header", "focused"])
+    focused: state.getIn(["header", "focused"]),
     // focused: state.get("header").get("focused")
+    list: state.getIn(["header", "list"]),
+    page: state.getIn(["header", "page"]),
+    totalPage: state.getIn(["header", "totalPage"]),
+    mouseIn: state.getIn(["header", "mouseIn"])
   };
 };
 
 const mapDispathToProps = dispatch => {
   return {
-    handleInputFocus() {
+    handleInputFocus(list) {
+      list.size === 0 && dispatch(actionCreators.getList());
+
       dispatch(actionCreators.searchFocus());
     },
     handleInputBlur() {
       dispatch(actionCreators.searchBlur());
+    },
+    handleMouseEnter() {
+      dispatch(actionCreators.mouseEnter());
+    },
+    handleMouseLeave() {
+      dispatch(actionCreators.mouseLeave());
+    },
+    handleChangePage(page, totalPage, spin) {
+      let originAgle = spin.style.transform.replace(/[^0-9]/gi, "");
+      if (originAgle) {
+        originAgle = parseInt(originAgle, 10);
+      } else {
+        originAgle = 0;
+      }
+      spin.style.transform = "rotate(" + (originAgle + 360) + "deg)";
+      if (page < totalPage) {
+        dispatch(actionCreators.changePage(page + 1));
+      } else {
+        dispatch(actionCreators.changePage(1));
+      }
     }
   };
 };
